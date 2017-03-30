70f979e82e5f9624aef7a9598b7a373924b6ddc9
jetsasank <jet.sasank@gmail.com>
2017-03-30 15:39:54 -0400

    Problem 1

---
+++ Problem1/Solution.java
@@ -0,0 +1,23 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class Solution {
+
+	public int[] twoSum(int[] nums, int target) {
+		int[] result = new int[2];
+		Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+		for (int i = 0; i < nums.length; i++) {
+			int num = nums[i];
+			int find = target - num;
+			if (map.containsKey(find)) {
+				result[0] = i;
+				result[1] = map.get(find);
+				break;
+			}
+			map.put(num, i);
+		}
+
+		return result;
+	}
+
+}
