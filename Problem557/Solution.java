99da33423e214d4fbe7d52436699f4e55df5f31e
jet <gteja231989@gmail.com>
2017-04-09 15:25:22 -0400

    Problem 557

---
+++ Problem557/Solution.java
@@ -0,0 +1,26 @@
+public class Solution {
+	public String reverseWords(String str) {
+		if (str == null || str.length() == 0) {
+			return str;
+		}
+		int n = str.length();
+		StringBuilder reverse = new StringBuilder();
+		StringBuilder reverseWord = new StringBuilder();
+
+		for (int i = 0; i < n; i++) {
+			if (str.charAt(i) != ' ') {
+				reverseWord.append(str.charAt(i));
+			}
+
+			if ((str.charAt(i) == ' ') || (i == n - 1)) {
+				reverseWord = reverseWord.reverse();
+				reverse.append(reverseWord);
+				if (i != n - 1) {
+					reverse.append(" ");
+				}
+				reverseWord = new StringBuilder();
+			}
+		}
+		return reverse.toString();
+	}
+}
