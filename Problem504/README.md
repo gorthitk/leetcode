5c9955236230dcf83ab828a352eef3cbceda7838
jet <gteja231989@gmail.com>
2017-05-17 16:28:50 -0400

    Problem504

---
+++ Problem504/README.md
@@ -0,0 +1,7 @@
+### 504. Base 7
+
+### Description 
+	Given an integer, return its base 7 string representation.
+
+### LeetCode URL 
+	https://leetcode.com/problems/base-7/#/description
---
+++ Problem504/Solution.java
@@ -0,0 +1,13 @@
+public class Solution {
+	public String convertToBase7(int num) {
+		boolean neg = num < 0;
+		num = Math.abs(num);
+		StringBuilder sb = new StringBuilder();
+		if (num == 0) sb.append(0);
+		while (num > 0) {
+			sb.append(num % 7);
+			num /= 7;
+		}
+		return neg ? "-" + sb.reverse().toString() : sb.reverse().toString();
+	}
+}
