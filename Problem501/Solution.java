a5b943311ee2155760d4975185fe7e5b2a9a4f4e
Teja <tgorthi@travelclick.com>
2017-05-17 01:25:29 -0400

    Problem501

---
+++ Problem501/Solution.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+public class Solution {
+	private int maxCount;
+	private int count;
+	private int curr;
+
+	public int[] findMode(TreeNode root) {
+		List<Integer> result = new ArrayList<>();
+		maxCount = 0;
+		count = 0;
+		curr = Integer.MAX_VALUE;
+
+		inOrder(result, root);
+
+		int n = result.size();
+		int[] res = new int[n];
+		for (int i = 0; i < n; i++) {
+			res[i] = result.get(i);
+		}
+		return res;
+	}
+
+	private void inOrder(List<Integer> result, TreeNode root) {
+		if (root == null)
+			return;
+		inOrder(result, root.left);
+		count = root.val == curr ? count + 1 : 1;
+		curr = root.val;
+		if (count > maxCount) {
+			result.clear();
+		}
+		maxCount = count >= maxCount ? count : maxCount;
+		if (count >= maxCount) {
+			result.add(curr);
+		}
+
+		inOrder(result, root.right);
+	}
+}
