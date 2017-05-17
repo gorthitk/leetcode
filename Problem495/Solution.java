746010bf8ec23cbe2e5358b813d9abf9b9204db9
jet <gteja231989@gmail.com>
2017-05-17 16:58:23 -0400

    Problem495

---
+++ Problem495/Solution.java
@@ -0,0 +1,18 @@
+public class Solution {
+
+	public int findPoisonedDuration(int[] timeSeries, int duration) {
+		if (timeSeries == null || timeSeries.length == 0)
+			return 0;
+		int time = 0;
+		int poisonedTimeStart = timeSeries[0];
+		int poisonedTimeEnd = timeSeries[0] + duration;
+		for (int i = 1; i < timeSeries.length; i++) {
+			if (timeSeries[i] > poisonedTimeEnd) {
+				time += (poisonedTimeEnd - poisonedTimeStart);
+				poisonedTimeStart = timeSeries[i];
+			}
+			poisonedTimeEnd = timeSeries[i] + duration;
+		}
+		return time + (poisonedTimeEnd - poisonedTimeStart);
+	}
+}
