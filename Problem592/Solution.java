0a9dfbd123b84c198c708fedd543d79cdbc864a1
jet <gteja231989@gmail.com>
2017-05-20 23:04:36 -0400

    Problem592/593

---
+++ Problem592/Solution.java
@@ -0,0 +1,67 @@
+import java.util.*;
+
+public class Solution {
+
+ 	public String fractionAddition(String expression) {
+		if (expression == null || expression.length() < 3)
+			return "";
+
+		List<Integer> numerators = new ArrayList<>();
+		List<Integer> denominators = new ArrayList<>();
+		for (int i = 0; i < expression.length(); i++) {
+			if (expression.charAt(i) == '/') {
+				int denominator = 0;
+				int j = i+1;
+				while(j < expression.length() && Character.isDigit(expression.charAt(j))) {
+					denominator = denominator*10 + expression.charAt(j) - '0';
+					j++;
+				}
+				j = i-1;
+				int numerator = 0;
+				StringBuilder num = new StringBuilder();
+				while(j>= 0 && Character.isDigit(expression.charAt(j))) {
+					num.append("" +expression.charAt(j));
+					j--;
+				}
+				num.reverse();
+				numerator = Integer.parseInt(num.toString());
+				if ( j>= 0) {
+					if (expression.charAt(j) == '-') {
+						numerator *= -1;
+					}
+				}
+				numerators.add(numerator);
+				denominators.add(denominator);
+			}
+		}
+		long result_denominator = getLCM(denominators);
+		long result_numerator = 0;
+		for (int i = 0; i < numerators.size(); i++) {
+			result_numerator += numerators.get(i) * (result_denominator/denominators.get(i));
+		}
+		long gcd = getGCD(Math.abs(result_numerator), result_denominator);
+		result_denominator = result_denominator/gcd;
+		result_numerator = result_numerator/gcd;
+		return "" + result_numerator + "/" + result_denominator;
+	}
+
+	private long getLCM(List<Integer> denominators) {
+		long lcm = denominators.get(0);
+		for (int i =1; i < denominators.size(); i++) {
+			lcm = getLCM(lcm, denominators.get(i));
+		}
+		return lcm;
+	}
+	private  long getGCD(long a, long b) {
+		while (b > 0) {
+			long temp = b;
+			b = a % b;
+			a = temp;
+		}
+		return a;
+	}
+
+	private  long getLCM(long a, long b) {
+		return a * (b / getGCD(a, b));
+	}
+}
---
+++ Problem593/Solution.java
@@ -0,0 +1,39 @@
+public class Solution {
+	public boolean validSquare(int[] p1, int[] p2, int[] p3, int[] p4) {
+		if (same(p1, p2, p3, p4))
+			return false;
+		double d1 = squareDistance(p1, p2);
+		double d2 = squareDistance(p1, p3);
+		double d3 = squareDistance(p1, p4);
+
+		if (d1 == d2 && 2 * d1 == d3) {
+			double d = squareDistance(p2, p4);
+			return (d == squareDistance(p3, p4) && d == d1);
+		}
+
+		if (d2 == d3 && 2 * d2 == d1) {
+			double d = squareDistance(p2, p3);
+			return (d == squareDistance(p2, p4) && d == d2);
+		}
+		if (d1 == d3 && 2 * d1 == d2) {
+			double d = squareDistance(p2, p3);
+			return (d == squareDistance(p3, p4) && d == d1);
+		}
+
+		return false;
+	}
+
+	private boolean same(int[] p1, int[] p2, int[] p3, int[] p4) {
+
+		return compare(p1, p2) || compare(p1, p3) || compare(p1, p4) || compare(p2, p3) || compare(p2, p4)
+				|| compare(p3, p4);
+	}
+
+	private boolean compare(int[] p1, int[] p2) {
+		return p1[0] == p2[0] && p1[1] == p2[1];
+	}
+
+	private double squareDistance(int[] point1, int[] point2) {
+		return Math.pow(point1[0] - point2[0], 2) + Math.pow(point1[1] - point2[1], 2);
+	}
+}
