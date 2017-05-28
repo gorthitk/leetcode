c6e19bcb1f0c211cfa5338229dfe0cb542e71830
jetsasank <gteja231989@gmail.com>
2017-05-28 16:20:49 -0400

    Problem131

---
+++ Problem131/README.md
@@ -0,0 +1,8 @@
+### Problem 131. Palindrome Partitioning
+
+### Description
+	Given a string s, partition s such that every substring of the partition is a palindrome.
+	Return all possible palindrome partitioning of s.
+
+### LeetCode URL
+	https://leetcode.com/problems/palindrome-partitioning/#/description
---
+++ Problem131/Solution.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution {
+	public List<List<String>> partition(String s) {
+		List<List<String>> result = new ArrayList<List<String>>();
+		getPartitions(s, result, 0, new ArrayList<>());
+		return result;
+	}
+
+	private void getPartitions(String s, List<List<String>> result, int idx, List<String> arrayList) {
+		if (idx >= s.length()) {
+			result.add(new ArrayList<>(arrayList));
+			return;
+		}
+		for (int i = idx; i < s.length(); i++) {
+			if (isPalindrome(s, idx, i)) {
+				arrayList.add(s.substring(idx, i+1));
+				getPartitions(s, result, i+1, arrayList);
+				arrayList.remove(arrayList.size()-1);
+			}
+		}
+	}
+	private boolean isPalindrome(String s, int i, int j) {
+		while (i < j) {
+			if (s.charAt(i) != s.charAt(j)) return false;
+			i++;
+			j--;
+		}
+		return true;
+	}
+}
