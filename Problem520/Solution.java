d13c7aee2d63a1fa61353d69e377dbee301941ce
jetsasank <gteja231989@gmail.com>
2017-05-29 19:37:14 -0400

    Problem 520

--- Problem520/Solution.java
+++ Problem520/Solution.java
@@ -1,26 +1,8 @@
+import java.util.regex.Pattern;
+
 public class Solution {
-	private int count = 0;
-
-	public int countArrangement(int N) {
-		generateArrangment(new boolean[N+1], 1, N);
-		return count;
-	}
-
-	private void generateArrangment(boolean[] visited, int curr, int N) {
-		if (curr > N) {
-			count++;
-			return;
-		}
-		for (int i = 1; i <= N; i++) {
-			if (!visited[i] && posSatisfied(curr, i)) {
-				visited[i] = true;
-				generateArrangment(visited, curr + 1, N);
-				visited[i] = false;
-			}
-		}
-	}
-
-	private boolean posSatisfied(int i, int num) {
-		return num % i == 0 || i % num == 0;
+	public boolean detectCapitalUse(String word) {
+		return Pattern.compile("[A-Z]+").matcher(word).matches() || Pattern.compile("[a-z]+").matcher(word).matches()
+				|| Pattern.compile("[A-Z][a-z]+").matcher(word).matches();
 	}
 }
