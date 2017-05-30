244fb67bf08b8685dd38bb98e690ecf31f500f6e
jetsasank <gteja231989@gmail.com>
2017-05-30 17:29:37 -0400

    Problem162

---
+++ Problem162/Solution.java
@@ -0,0 +1,10 @@
+public class Solution {
+	public int findPeakElement(int[] nums) {
+		int n = nums.length;
+		int idx = nums.length == 1 ? 0 : nums[0] > nums[1] ? 0 : nums[n - 1] > nums[n - 2] ? n - 1 : -1;
+		for (int i = 0; idx == -1 && i < n; i++) {
+			idx = i != 0 && i != n - 1 && nums[i] > nums[i - 1] && nums[i] > nums[i + 1] ? i : idx;
+		}
+		return idx;
+	}
+}
