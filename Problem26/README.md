df420a92099594e1667c021f388bb9434a924711
jetsasank <gteja231989@gmail.com>
2017-05-31 17:06:08 -0400

    Problem 80/26

---
+++ Problem26/README.md
@@ -0,0 +1,7 @@
+### 26. Remove Duplicates from Sorted Array
+
+### Description
+	Given a sorted array, remove the duplicates in place such that each element appear only once and return the new length.
+
+### LeetCode URL
+	https://leetcode.com/problems/remove-duplicates-from-sorted-array/#/description
---
+++ Problem80/Solution.java
@@ -0,0 +1,15 @@
+public class Solution {
+	public int removeDuplicates(int[] nums) {
+		int i = 0;
+		int start = 0;
+		for (int j = 1; j < nums.length; j++) {
+			if (nums[j] != nums[i] || (nums[j] == nums[i] && j - start == 1) ) {
+				start = nums[j] != nums[i] ? j : start;
+				i++;
+				nums[i] = nums[j];
+				
+			} 
+		}
+		return i + 1;
+	}
+}
