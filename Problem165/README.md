5d0866fbcceb937a00bf1a3350d5bbecb0b2eb70
jetsasank <gteja231989@gmail.com>
2017-05-31 11:24:15 -0400

    Problem165

---
+++ Problem165/README.md
@@ -0,0 +1,8 @@
+### Problem 165. Compare Version Numbers
+
+### Description 
+	Compare two version numbers version1 and version2.
+	If version1 > version2 return 1, if version1 < version2 return -1, otherwise return 0.
+
+### LeetCode URL 
+	https://leetcode.com/problems/compare-version-numbers/#/description
---
+++ Problem165/Solution.java
@@ -0,0 +1,14 @@
+public class Solution {
+	public int compareVersion(String version1, String version2) {
+		Queue<String> ver1 = new LinkedList<String>(Arrays.asList(version1.split("\\.")));
+		Queue<String> ver2 = new LinkedList<String>(Arrays.asList(version2.split("\\.")));
+
+		while (!ver1.isEmpty() || !ver2.isEmpty()) {
+			int num1 = ver1.isEmpty() ? 0 : ver1.peek().isEmpty() ? 0 : Integer.parseInt(ver1.poll());
+			int num2 = ver2.isEmpty() ? 0 : ver2.peek().isEmpty() ? 0 : Integer.parseInt(ver2.poll());
+			if (num1 > num2) return 1;
+			if (num1 < num2) return -1;
+		}
+		return 0;
+	}
+}
