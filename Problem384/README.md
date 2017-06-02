745562a27e0ebda5276805ce26f54572209210d2
jetsasank <gteja231989@gmail.com>
2017-06-02 01:08:32 -0400

    Problem 384

---
+++ Problem384/README.md
@@ -0,0 +1,7 @@
+### Problem 384. Shuffle an Array
+
+### Description
+	Shuffle a set of numbers without duplicates.
+
+### LeetCode URL
+	https://leetcode.com/problems/shuffle-an-array/#/description
---
+++ Problem384/Solution.java
@@ -0,0 +1,17 @@
+public class Solution {
+	public boolean isSubsequence(String s, String t) {
+		if (s.length() > t.length())
+			return false;
+		int i = 0;
+		int j = 0;
+		while (i < s.length()) {
+			while (j < t.length() && t.charAt(j) != s.charAt(i))
+				j++;
+			if (j >= t.length())
+				return false;
+			i++;
+			j++;
+		}
+		return true;
+	}
+}
