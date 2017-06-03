e615249ab7f004a4c0d3f68c5702ab1915526b47
jetsasank <gteja231989@gmail.com>
2017-06-03 05:35:53 -0400

    Problem216

---
+++ Problem216/README.md
@@ -0,0 +1,7 @@
+### Problem 216. Combination Sum III
+
+### Description 
+	Find all possible combinations of k numbers that add up to a number n, given that only numbers from 1 to 9 can be used and each combination should be a unique set of numbers.
+
+### LeetCode URL 
+	https://leetcode.com/problems/combination-sum-iii/#/description
---
+++ Problem216/Solution.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution {
+	public List<List<Integer>> combinationSum3(int k, int n) {
+		List<List<Integer>> result = new ArrayList<>();
+		getCombinations(n, 0, new ArrayList<>(), result, 1, k);
+		return result;
+	}
+
+	private void getCombinations(int target, int currSum, List<Integer> curr, List<List<Integer>> result, int idx,
+			int k) {
+		if (currSum > target || curr.size() > k) {
+			return;
+		}
+		if (currSum == target && curr.size() == k) {
+			result.add(new ArrayList<>(curr));
+			return;
+		}
+
+		for (int i = idx; i < 10; i++) {
+			curr.add(i);
+			getCombinations(target, currSum + i, curr, result, i+1, k);
+			curr.remove(curr.size() - 1);
+		}
+	}
+}
