97305d0afa5e32de4b6d8fdb07611585e9396ac1
jetsasank <gteja231989@gmail.com>
2017-06-03 05:28:25 -0400

    Problem39

---
+++ Problem39/Solution.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution {
+	public List<List<Integer>> combinationSum(int[] candidates, int target) {
+		List<List<Integer>> result = new ArrayList<>();
+		getCombinations(candidates, target, 0, new ArrayList<>(), result, 0);
+		return result;
+	}
+
+	private void getCombinations(int[] candidates, int target, int currSum, List<Integer> curr,
+			List<List<Integer>> result, int idx) {
+		if (currSum == target) {
+			result.add(new ArrayList<>(curr));
+			return;
+		}
+		if (currSum > target) {
+			return;
+		}
+		for (int i = idx; i < candidates.length; i++) {
+			curr.add(candidates[i]);
+			getCombinations(candidates, target, currSum + candidates[i], curr, result, i);
+			curr.remove(curr.size() - 1);
+		}
+	}
+}
