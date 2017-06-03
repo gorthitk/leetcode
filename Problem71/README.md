2dc4e642eb57b43fa850263fde289653e8f28571
jetsasank <gteja231989@gmail.com>
2017-06-03 04:52:06 -0400

    Problem71

---
+++ Problem71/README.md
@@ -0,0 +1,7 @@
+### Problem 71. Simplify Path
+
+### Description
+	Given an absolute path for a file (Unix-style), simplify it.
+
+### LeetCode URL
+	https://leetcode.com/problems/simplify-path/#/description
---
+++ Problem71/Solution.java
@@ -0,0 +1,23 @@
+import java.util.Stack;
+
+public class Solution {
+	public String simplifyPath(String path) {
+		Stack<String> dirs = new Stack<>();
+		for (String dir : path.split("/")) {
+			if (dir.equals("..") && !dirs.isEmpty()) {
+				dirs.pop();
+			} else {
+				if (dir.equals(".") || dir.equals("..") || dir.equals("")) {
+					continue;
+				}
+				dirs.push(dir);
+			}
+		}
+		String simplifiedPath = "";
+		while (!dirs.isEmpty()) {
+			simplifiedPath = "/" + dirs.pop() + simplifiedPath;
+		}
+		return simplifiedPath.isEmpty() ? "/" : simplifiedPath;
+	}
+
+}
