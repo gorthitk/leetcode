da9551ed94db410c085090915e8edb79ac4d6012
jetsasank <gteja231989@gmail.com>
2017-06-04 18:37:02 -0400

    Problem118

---
+++ Problem118/README.md
@@ -0,0 +1,9 @@
+### Problem 118. Pascal's Triangle
+
+### Description
+	Given numRows, generate the first numRows of Pascal's triangle.
+
+Refer to Problem 116
+### LeetCode URL
+	https://leetcode.com/problems/pascals-triangle/#/description
+	
---
+++ Problem118/Solution.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class Solution {
+	public List<List<Integer>> generate(int numRows) {
+		List<List<Integer>> result = new ArrayList<>();
+		List<Integer> level1 = Arrays.asList(1);
+		List<Integer> level2 = Arrays.asList(1, 1);
+		if (numRows <= 0) {
+			return result;
+		}
+		result.add(level1);
+		if (numRows == 1) {
+			return result;
+		}
+		result.add(level2);
+		if (numRows == 2) {
+			return result;
+		}
+		for (int i = 2; i <= numRows; i++) {
+			List<Integer> pascalLvl = new ArrayList<>();
+			pascalLvl.add(1);
+			for (int j = 1; j < i; j++) {
+				pascalLvl.add(result.get(i - 1).get(j) + result.get(i - 1).get(j - 1));
+			}
+			pascalLvl.add(1);
+			result.add(pascalLvl);
+		}
+		return result;
+	}
+}
