5e500819f39ecdbba16d76e1b760ce4d5b360827
jetsasank <gteja231989@gmail.com>
2017-06-06 06:11:46 -0400

    Problem606

---
+++ Problem606/Solution.java
@@ -0,0 +1,19 @@
+public class Solution {
+	public String tree2str(TreeNode t) {
+		if (t == null)
+			return "";
+		StringBuilder sb = new StringBuilder();
+		sb.append(t.val);
+		if (t.left == null && t.right == null)
+			return sb.toString();
+		sb.append("(");
+		sb.append(tree2str(t.left));
+		sb.append(")");
+		if (t.right != null) {
+			sb.append("(");
+			sb.append(tree2str(t.right));
+			sb.append(")");
+		}
+		return sb.toString();
+	}
+}
