b9becd07e0c51d2ad899cc9ea017eb912c20b006
jetsasank <gteja231989@gmail.com>
2017-06-06 07:10:45 -0400

    Problem38

---
+++ Problem38/README.md
@@ -0,0 +1,8 @@
+### Problem 38. Count and Say
+
+### Description
+	The count-and-say sequence is the sequence of integers beginning as follows:
+	1, 11, 21, 1211, 111221, ...
+
+### LeetCode URL
+	https://leetcode.com/problems/count-and-say/#/description
---
+++ Problem38/Solution.java
@@ -0,0 +1,24 @@
+public class Solution {
+	public String countAndSay(int n) {
+		if (n == 1)
+			return "1";
+		String prev = countAndSay(n - 1);
+		StringBuilder sb = new StringBuilder();
+		char c = prev.charAt(0);
+		int count = 1;
+		for (int i = 1; i < prev.length(); i++) {
+			char current = prev.charAt(i);
+			if (current == c) {
+				count++;
+			} else {
+				sb.append(count);
+				sb.append(c);
+				c = current;
+				count = 1;
+			}
+		}
+		sb.append(count);
+		sb.append(c);
+		return sb.toString();
+	}
+}
