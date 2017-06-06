5772a2eac82bbed995e98b208b33acaddbc8d0a6
jetsasank <gteja231989@gmail.com>
2017-06-06 06:47:58 -0400

    Problem609

---
+++ Problem609/Solution.java
@@ -0,0 +1,24 @@
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Solution {
+	public List<List<String>> findDuplicate(String[] paths) {
+		Map<String, List<String>> map = new HashMap<>();
+		for (String path : paths) {
+			findDuplicates(path, map);
+		}
+		return map.values().stream().filter(a -> a.size() > 1).collect(Collectors.toList());
+	}
+
+	private void findDuplicates(String path, Map<String, List<String>> map) {
+		String[] tokens = path.split(" ");
+		String dirPath = tokens[0];
+		for (int i = 1; i < tokens.length; i++) {
+			String file = tokens[i].substring(0, tokens[i].indexOf("("));
+			String content = tokens[i].substring(tokens[i].indexOf("(")+1, tokens[i].indexOf(")"));
+			map.putIfAbsent(content, new ArrayList<>());
+			String filePath = dirPath + "/" + file;
+			map.get(content).add(filePath);
+		}
+	}
+}
