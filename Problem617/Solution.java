f6c6ea7852e76731a319b7a1ffa1e19bc0e1ed4a
jetsasank <gteja231989@gmail.com>
2017-06-11 04:09:15 -0400

    Problem617

---
+++ Problem617/Solution.java
@@ -0,0 +1,11 @@
+public class Solution {
+	public TreeNode mergeTrees(TreeNode t1, TreeNode t2) {
+		TreeNode root = null;
+		if (t1 == null && t2 == null)
+			return root;
+		root = new TreeNode((t1 == null ? 0 : t1.val) + (t2 == null ? 0 : t2.val));
+		root.left = mergeTrees(t1 == null ? null : t1.left, t2 == null ? null : t2.left);
+		root.right = mergeTrees(t1 == null ? null : t1.right, t2== null ? null : t2.right);
+		return root;
+	}
+}
