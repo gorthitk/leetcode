d6e8958dc739a1d8bd2eee702ff1e8f41e2dedcc
jetsasank <gteja231989@gmail.com>
2017-06-16 00:33:50 -0400

    Problem331

---
+++ Problem331/Solution.java
@@ -0,0 +1,11 @@
+public class Solution {
+	public boolean isValidSerialization(String preorder) {
+		String[] nodes = preorder.split(",");
+		int expectedNodes = 1;
+		for (String node : nodes) {
+			if(--expectedNodes < 0) return false;
+			expectedNodes += node.equals("#") ? 0 : 2;
+		}
+		return expectedNodes == 0;
+	}
+}
