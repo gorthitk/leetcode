ca6b7b74ce62107d80378d45e008c0c96f4a8530
jetsasank <gteja231989@gmail.com>
2017-06-19 16:00:49 -0400

    Problem624

---
+++ Problem624/README.md
@@ -0,0 +1,7 @@
+### Problem 624. Maximum Distance in Arrays
+
+### Description
+	Given m arrays, and each array is sorted in ascending order. Now you can pick up two integers from two different arrays (each array picks one) and calculate the distance. We define the distance between two integers a and b to be their absolute difference |a-b|. Your task is to find the maximum distance.
+
+### LeetCode URL
+	https://leetcode.com/problems/maximum-distance-in-arrays/#/description
---
+++ Problem624/Solution.java
@@ -0,0 +1,19 @@
+import java.util.List;
+
+public class Solution {
+	public int maxDistance(List<List<Integer>> arrays) {
+		int currMin = arrays.get(0).get(0);
+		int currMax = arrays.get(0).get(arrays.get(0).size() - 1);
+		int result = Integer.MIN_VALUE;
+		for (int i = 1; i < arrays.size(); i++) {
+			int min = arrays.get(i).get(0);
+			int max = arrays.get(i).get(arrays.get(i).size() - 1);
+			result = Math.max(result, Math.abs(currMin - max));
+			result = Math.max(result, Math.abs(currMax - min));
+
+			currMax = Math.max(currMax, max);
+			currMin = Math.min(currMin, min);
+		}
+		return result;
+	}
+}
