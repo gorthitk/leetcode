789dd5a6bf21d6efd0db40fcc83c8fef781af731
jetsasank <gteja231989@gmail.com>
2017-06-19 17:21:56 -0400

    Updating Comments.

--- Problem72/Solution.java
+++ Problem72/Solution.java
@@ -1,10 +1,13 @@
 public class Solution {
 	public int minDistance(String word1, String word2) {
 		int[][] dp = new int[word1.length() + 1][word2.length() + 1];
+		
+		//Number of Operation to Convert word1 to Empty String. (Deletes Required)
 		for (int i = 0; i <= word1.length(); i++) {
 			dp[i][0] = i;
 		}
 		
+		//Number of Operation to Convert Empty String to word2. (Inserts Required)
 		for (int i = 0; i <= word2.length(); i++) {
 			dp[0][i] = i;
 		}
