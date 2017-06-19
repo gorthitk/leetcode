8acde5a7cc8f84e7988898d35f2307e5680f5cc9
jetsasank <gteja231989@gmail.com>
2017-06-19 16:57:20 -0400

    Problem583

---
+++ Problem583/README.md
@@ -0,0 +1,7 @@
+### Problem 583. Delete Operation for Two Strings
+
+### Description
+	Given two words word1 and word2, find the minimum number of steps required to make word1 and word2 the same, where in each step you can delete one character in either string.
+
+### LeetCode URL
+	https://leetcode.com/problems/delete-operation-for-two-strings/#/description
---
+++ Problem583/Solution.java
@@ -0,0 +1,11 @@
+public class Solution {
+	public int minDistance(String word1, String word2) {
+		int[][] dp = new int[word1.length() + 1][word2.length() + 1]; // Find Length of Common Subsequence
+		for (int i = 0; i <= word1.length(); i++) {
+			for (int j = 0; j <= word2.length(); j++) {
+				dp[i][j] = i == 0 || j == 0 ? 0 : word1.charAt(i-1) == word2.charAt(j-1) ? 1 + dp[i-1][j-1] : Math.max(dp[i-1][j], dp[i][j-1]);
+			}
+		}
+		return word1.length() + word2.length() - 2*dp[word1.length()][word2.length()];
+	}
+}
