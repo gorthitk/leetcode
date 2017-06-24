c079e8634ef782f613b65f5d49f59357f5b5450d
jetsasank <gteja231989@gmail.com>
2017-06-23 21:56:15 -0400

    Problem169

---
+++ Problem169/Solution.java
@@ -0,0 +1,14 @@
+public class Solution {
+    public int majorityElement(int[] nums) {
+        int count = 1;
+        int elementIdx = 0;
+        for (int i = 1; i < nums.length; i++) {
+             count = nums[i] == nums[elementIdx] ? count+1 : count-1;
+             if (count < 0) {
+                 elementIdx = i;
+                 count = 1;
+             }
+        }
+        return nums[elementIdx];
+    }
+}
