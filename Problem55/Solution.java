001fc5370ee9d941f5ea040b40804326190f6031
jetsasank <gteja231989@gmail.com>
2017-06-28 07:54:59 -0400

    Problem55

---
+++ Problem55/Solution.java
@@ -0,0 +1,9 @@
+public class Solution {
+	public boolean canJump(int[] nums) {
+		int max = 0;
+		for (int i = 0; i < nums.length && i <= max; i++) {
+			max = Math.max(max, i+nums[i]);
+		}
+		return max >= nums.length-1;
+	}
+}
