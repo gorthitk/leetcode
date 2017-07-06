7526a6641b5cc2ad04cb5e3c2e1dcaedc8157eab
jetsasank <gteja231989@gmail.com>
2017-07-06 14:45:30 -0400

    Updating Solution

--- Problem129/Solution.java
+++ Problem129/Solution.java
@@ -1,28 +1,17 @@
-import Solution.TreeNode;
-
 public class Solution {
 	private int sum = 0;
 	public int sumNumbers(TreeNode root) {
 		getSum(root, 0);
 		return sum;
 	}
-	private void getSum(TreeNode root, int curr) {
-		if(root == null) return;
-		curr = curr*10 + root.val;
+	private void getSum(TreeNode root, int prevSum) {
+		if (root == null) return;
+		int currentSum = prevSum*10 + root.val;
 		if (root.left == null && root.right == null) {
-			sum += curr;
+			sum += currentSum;
 			return;
 		}
-		getSum(root.left, curr);
-		getSum(root.right, curr);
-	}
-	public static class TreeNode {
-		int val;
-		TreeNode left;
-		TreeNode right;
-
-		TreeNode(int x) {
-			val = x;
-		}
+		getSum(root.left, currentSum);
+		getSum(root.right, currentSum);
 	}
 }
