fc7ed97c3c909a6eeba1f0b8de24acfb2bc344cc
jetsasank <gteja231989@gmail.com>
2017-07-06 13:54:38 -0400

    Problem101

---
+++ Problem101/README.md
@@ -0,0 +1,7 @@
+### Problem 101. Symmetric Tree
+
+### Description 
+	Given a binary tree, check whether it is a mirror of itself (ie, symmetric around its center).
+
+### LeetCode URL 
+	https://leetcode.com/problems/symmetric-tree/#/description
---
+++ Problem101/Solution.java
@@ -0,0 +1,10 @@
+public class Solution {
+	public boolean isSymmetric(TreeNode root) {
+		return root == null || isSymmetric(root.left, root.right);
+	}
+
+	private boolean isSymmetric(TreeNode left, TreeNode right) {
+		return left == null || right == null ? left == right
+				: left.val == right.val && isSymmetric(left.left, right.right) && isSymmetric(left.right, right.left);
+	}
+}
