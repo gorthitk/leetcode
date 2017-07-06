838edf0433997f808e8203a11df71cf1d8528ff1
jetsasank <gteja231989@gmail.com>
2017-07-06 19:10:36 -0400

    Problem236

---
+++ Problem236/README.md
@@ -0,0 +1,7 @@
+### Problem 236. Lowest Common Ancestor of a Binary Tree
+
+### Description 
+	Given a binary tree, find the lowest common ancestor (LCA) of two given nodes in the tree.
+
+### LeetCode URL 
+	https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-tree/#/description
---
+++ Problem236/Solution.java
@@ -0,0 +1,8 @@
+public class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+      if (root == null || p == root || q == root) return root;
+      TreeNode left = lowestCommonAncestor(root.left, p, q);
+      TreeNode right = lowestCommonAncestor(root.right, p, q);
+      return left != null && right != null ? root : left != null ? left : right;
+    }
+}
