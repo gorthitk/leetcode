8ce124140f5db7758b08438ea21697525b740d38
jetsasank <gteja231989@gmail.com>
2017-07-06 11:59:46 -0400

    Problem94

---
+++ Problem94/README.md
@@ -0,0 +1,7 @@
+### Problem 94. Binary Tree Inorder Traversal
+
+### Description
+	Given a binary tree, return the inorder traversal of its nodes' values.
+
+### LeetCode URL
+	https://leetcode.com/problems/binary-tree-inorder-traversal/#/description
---
+++ Problem94/Solution.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Solution {
+	public List<Integer> inorderTraversal(TreeNode root) {
+		List<Integer> inorder = new ArrayList<>();
+		if (root != null) {
+			inorder.addAll(inorderTraversal(root.left));
+			inorder.add(root.val);
+			inorder.addAll(inorderTraversal(root.right));
+		}
+		return inorder;
+	}
+}
