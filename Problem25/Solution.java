c043e6a9eab9b510015da91e7712bbc63db0f0a7
jetsasank <gteja231989@gmail.com>
2017-07-17 14:21:50 -0400

    Problem25

---
+++ Problem25/Solution.java
@@ -0,0 +1,30 @@
+public class Solution {
+	public ListNode reverseKGroup(ListNode head, int k) {
+		ListNode ptr = head;
+		int count = k;
+		ListNode prev = null;
+
+		while (ptr != null && count > 0) {
+			prev = ptr;
+			ptr = ptr.next;
+			count--;
+		}
+
+		if (count != 0)
+			return head;
+
+		prev.next = null;
+		ListNode newHead = reverse(head);
+		head.next = reverseKGroup(ptr, k);
+		return newHead;
+	}
+
+	private ListNode reverse(ListNode head) {
+		if (head == null || head.next == null) return head;
+		ListNode next = head.next;
+		head.next = null;
+		ListNode newHead = reverse(next);
+		next.next = head;
+		return newHead;
+	}
+}
