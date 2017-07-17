c8759f93797fff912606cf892f0675cd5e6916ff
jetsasank <gteja231989@gmail.com>
2017-07-17 14:41:23 -0400

    Problem28

---
+++ Problem28/Solution.java
@@ -0,0 +1,17 @@
+public class Solution {
+	public int strStr(String haystack, String needle) {
+		if (haystack.equals(needle))
+			return 0;
+		for (int i = 0; i < haystack.length() - needle.length(); i++) {
+			int k = i;
+			int j = 0;
+			while (j < needle.length() && k < haystack.length() && haystack.charAt(k) == needle.charAt(j)) {
+				k++;
+				j++;
+			}
+			if (j == needle.length())
+				return i;
+		}
+		return -1;
+	}
+}
