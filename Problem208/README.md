6dcc447327d890592eac172233e028f9fbb54790
jetsasank <gteja231989@gmail.com>
2017-07-18 11:33:44 -0400

    Problem 208

---
+++ Problem208/README.md
@@ -0,0 +1,7 @@
+### Problem 208. Implement Trie (Prefix Tree)
+
+### Description 
+	Implement a trie with insert, search, and startsWith methods.
+
+### LeetCode URL 
+	https://leetcode.com/problems/implement-trie-prefix-tree/#/description
---
+++ Problem208/Trie.java
@@ -0,0 +1,58 @@
+import Trie.Node;
+
+public class Trie {
+   Node root;
+
+   public Trie() {
+      root = new Node();
+   }
+
+   public void insert(String word) {
+      Node current = root;
+      for (int i = 0; i < word.length(); i++) {
+         char c = word.charAt(i);
+         if (current.children[c-'a'] == null) {
+            current.children[c-'a'] = new Node();
+         }
+         current = current.children[c-'a'];
+         if (i == word.length()-1) {
+            current.endOfWord = true;
+         }
+      }
+   }
+
+   public boolean search(String word) {
+      Node current = root;
+      for (int i = 0; i < word.length(); i++) {
+         char c = word.charAt(i);
+         if (current.children[c-'a'] == null) {
+            return false;
+         }
+         current = current.children[c-'a'];
+      }
+      return current.endOfWord;
+   }
+
+   public boolean startsWith(String prefix) {
+      Node current = root;
+      for (int i = 0; i < prefix.length(); i++) {
+         char c = prefix.charAt(i);
+         if (current.children[c-'a'] == null) {
+            return false;
+         }
+         current = current.children[c-'a'];
+      }
+      return true;
+   }
+
+   class Node {
+      Node[] children;
+      boolean endOfWord = false;
+
+      public Node() {
+         super();
+         children = new Node[26];
+      }
+
+   }
+}
