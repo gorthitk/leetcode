ceae497e25b60efae1a31b63d85b20933e94ab1e
jetsasank <gteja231989@gmail.com>
2017-07-19 14:30:39 -0400

    Refactoring the code

--- Problem5/Solution.java
+++ Problem5/Solution.java
@@ -1,28 +1,22 @@
 public class Solution {
 	public String longestPalindrome(String s) {
-		int start = 0;
-		int end = 0;
-		int longestPalindromeLen = 0;
+		int start = 0, end = 0, longest = 0;
 		for (int i = 0; i < s.length(); i++) {
-			int lenWithCenterAtChar = findPalindromeLength(s, i, i);
-			int lenWithCenterBetween = findPalindromeLength(s, i, i + 1);
-			int localLongestPalidromelen = Math.max(lenWithCenterAtChar, lenWithCenterBetween);
-			if (localLongestPalidromelen > longestPalindromeLen) {
-				longestPalindromeLen = localLongestPalidromelen;
-				start = i - (localLongestPalidromelen - 1) / 2;
-				end = i + (localLongestPalidromelen / 2);
+			int len = Math.max(getPalindomeLen(s, i, i + 1), getPalindomeLen(s, i, i));
+			if (len > longest) {
+				longest = len;
+				start = i - (len-1)/ 2;
+				end = i + len / 2;
 			}
 		}
-		return s.substring(start, end + 1);
+		return s.substring(start, end+1);
 	}
 
-	private int findPalindromeLength(String str, int start, int end) {
-		int len = 0;
-		while (start >= 0 && end < str.length() && str.charAt(start) == str.charAt(end)) {
-			end++;
-			start--;
+	private int getPalindomeLen(String s, int left, int right) {
+		while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
+			left--;
+			right++;
 		}
-		len = end - start - 1;
-		return len;
+		return right-left-1;
 	}
 }
