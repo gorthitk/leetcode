9e4ad81b03923e7651490d24c72d0e39ef42ed31
jetsasank <gteja231989@gmail.com>
2017-07-19 14:39:33 -0400

    Refactoring

--- Problem7/Solution.java
+++ Problem7/Solution.java
@@ -1,13 +1,10 @@
 public class Solution {
     public int reverse(int x) {
-		int reverse = 0;
-		while (x != 0) {
-			if (Math.abs(reverse) > 214748364) {
-				return 0;
-			}
-			reverse = reverse * 10 + x % 10;
-			x = x / 10;
-		}
-		return reverse;
+        long reverse = 0;
+        while (x != 0) {
+           reverse = reverse*10 + x%10;
+           x = x/10;
+        }
+        return reverse > Integer.MAX_VALUE || reverse < Integer.MIN_VALUE? 0 : (int) reverse;  
     }
 }
