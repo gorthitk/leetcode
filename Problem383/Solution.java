468cea2f0aae8eaa5150b86eebfb4504cad978e8
jetsasank <gteja231989@gmail.com>
2017-07-29 11:25:13 -0400

    Problem 383

---
+++ Problem383/Solution.java
@@ -0,0 +1,19 @@
+public class Solution {
+	public boolean canConstruct(String ransomNote, String magazine) {
+		if (ransomNote == null || magazine == null)
+			return ransomNote == magazine;
+		int[] counts = new int[26];
+		for (char c : ransomNote.toCharArray()) {
+			counts[c - 'a']++;
+		}
+
+		for (char c : magazine.toCharArray()) {
+			counts[c - 'a']--;
+		}
+		for (int i = 0; i < 26; i++) {
+			if (counts[i] > 0)
+				return false;
+		}
+		return true;
+	}
+}
