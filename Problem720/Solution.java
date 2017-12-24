e14d78d8e8366c263844dd6273a3e4abe8e6c393
jetsasank <gteja231989@gmail.com>
2017-12-24 01:04:30 -0500

    Problem 720

---
+++ Problem720/Solution.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+import java.util.*;
+
+class Solution {
+	public String longestWord(String[] words) {
+		String longestWord = "";
+		Set<String> set = new HashSet();
+		Arrays.sort(words);
+		for (String word : words)
+		{
+			String subString = word.substring(0, word.length()-1);
+			if (word.length() == 0 || set.contains(subString))
+			{
+				set.add(word);
+				if (longestWord.length() < word.length())
+				{
+					longestWord = word;
+				}
+			}
+		}
+
+
+		return longestWord;
+	}
+
+}
