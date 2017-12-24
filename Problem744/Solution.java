b0ee38ed6fe54e4799dec26da0efbfa110716440
jetsasank <gteja231989@gmail.com>
2017-12-23 23:55:30 -0500

    fixing wrong commit

--- Problem744/Solution.java
+++ Problem744/Solution.java
@@ -1,25 +1,6 @@
 class Solution {
-	public int minCostClimbingStairs(int[] cost) {
-		if (cost == null || cost.length == 0)
-		{
-			return Integer.MAX_VALUE;
-		}
+	public char nextGreatestLetter(char[] letters, char target) {
 
-		int len = cost.length;
-		if (len == 1 || len == 2)
-		{
-			return len == 1 ? cost[0] : Math.min(cost[1], cost[0]);
-		}
-
-		int[] dp = new int[len+1];
-		dp[0] = cost[0];
-		dp[1] = cost[1];
-
-		for (int i = 2; i <= len; i++)
-		{
-			dp[i] = Math.min(dp[i-1], dp[i-2]) + (i == len ? 0 : cost[i]);
-		}
-
-		return dp[len];
+		return (char) IntStream.range(0, letters.length).map(idx -> letters[idx]).filter(c -> c > target).findAny().orElse(letters[0]);
 	}
 }
