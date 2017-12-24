93426378f7135d2956d9ac64963fc326d6e11ef5
jetsasank <gteja231989@gmail.com>
2017-12-24 00:32:20 -0500

    Problem 724

---
+++ Problem724/Solution.java
@@ -0,0 +1,19 @@
+class Solution {
+	public int pivotIndex(int[] nums) {
+		int sum = 0;
+		for (int num : nums)
+		{
+			sum += num;
+		}
+		int left = 0;
+		for (int i = 0; i < nums.length; i++)
+		{
+			left = left + (i == 0 ? 0 : nums[i-1]);
+			if (left == sum - left - nums[i])
+			{
+				return i;
+			}
+		}
+		return -1;
+	}
+}
