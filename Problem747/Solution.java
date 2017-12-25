9fafafb1a04e6775a6dd86281b9b948b33245d5b
jetsasank <gteja231989@gmail.com>
2017-12-24 21:43:38 -0500

    Problem 747

---
+++ Problem747/Solution.java
@@ -0,0 +1,23 @@
+class Solution {
+	public int dominantIndex(int[] nums)
+	{
+		int max = 0, secondMax = 0, maxIdx = 0;
+
+		for (int i = 0; i < nums.length; i++)
+		{
+			int currNum = nums[i];
+			if (currNum > max)
+			{
+				secondMax = max;
+				max = currNum;
+				maxIdx = i;
+			}
+			else if (currNum > secondMax)
+			{
+				secondMax = currNum;
+			}
+		}
+
+		return max >= 2 * secondMax ? maxIdx : -1;
+	}
+}
