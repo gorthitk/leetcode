6a0720a255f1872d097d72bedbbfdeae0967c165
jetsasank <gteja231989@gmail.com>
2017-12-24 23:22:24 -0500

    Problem 653

---
+++ Problem653/Solution.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class Solution {
+	public boolean findTarget(TreeNode root, int k)
+	{
+		HashSet<Integer> set = new HashSet<>();
+		return dfs(root, k, set);
+	}
+
+	private boolean dfs(TreeNode root, int k, HashSet<Integer> set)
+	{
+		if (root == null)
+		{
+			return false;
+		}
+		if (set.contains(k-root.val))
+		{
+			return true;
+		}
+		set.add(root.val);
+		return dfs(root.left, k, set) || dfs(root.right, k, set);
+	}
+}
