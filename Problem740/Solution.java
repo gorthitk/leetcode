82f03b3aa76cf8797c66b126cb81f7a6f9718ce4
jetsasank <gteja231989@gmail.com>
2017-12-25 20:15:23 -0500

    Problem 740

---
+++ Problem740/Solution.java
@@ -0,0 +1,18 @@
+class Solution {
+	public int deleteAndEarn(int[] nums) {
+
+		int[] countsOfEachNumber = new int[10001];
+		for (int num : nums)
+		{
+			countsOfEachNumber[num] += num;
+		}
+
+		int[] maxEarnings = new int[10003];
+
+		for(int i = 10000; i >= 0; i--)
+		{
+			maxEarnings[i] = Math.max(countsOfEachNumber[i] + maxEarnings[i + 2], maxEarnings[i + 1]);
+		}
+		return maxEarnings[0];
+	}
+}
