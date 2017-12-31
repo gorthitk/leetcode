c5fb88dcacfffacbb2c78a10873a7a1de0ebd374
jetsasank <gteja231989@gmail.com>
2017-12-31 18:51:33 -0500

    Problem 671

---
+++ Problem671/Solution.java
@@ -0,0 +1,25 @@
+class Solution
+{
+    public int findSecondMinimumValue(TreeNode root)
+    {
+        if (root == null || root.left == null || root.right == null)
+        {
+            return -1;
+        }
+        int left = root.left.val, right = root.right.val;
+        if (root.left.val == root.val)
+        {
+            left = findSecondMinimumValue(root.left);
+        }
+        if (root.right.val == root.val)
+        {
+            right = findSecondMinimumValue(root.right);
+        }
+        if (left != -1 && right != -1)
+        {
+            return Math.min(left, right);
+        }
+
+        return left == -1 ? right : left;
+    }
+}
