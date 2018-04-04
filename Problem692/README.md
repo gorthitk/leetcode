c58b37f13bbda1a9fd641e1973b8699a05d14b46
jetsasank <gteja231989@gmail.com>
2018-04-04 21:37:22 +0530

    Problem 692

---
+++ Problem692/README.md
@@ -0,0 +1,9 @@
+### 692. Top K Frequent Words
+
+### Description
+    Given a non-empty list of words, return the k most frequent elements.
+    Your answer should be sorted by frequency from highest to lowest. 
+    If two words have the same frequency, then the word with the lower alphabetical order comes first.
+    
+### LeetCode URL
+    https://leetcode.com/problems/top-k-frequent-words/description/
---
+++ Problem692/Solution.java
@@ -0,0 +1,29 @@
+import java.util.*;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.Map.Entry;
+
+class Solution
+{
+    private static final Comparator<Entry<String, Long>> COMPARATOR =
+            (o1, o2) -> o1.getValue().equals(o2.getValue()) ? o1.getKey().compareTo(o2.getKey()) : Long.compare(o2.getValue(), o1.getValue());
+
+
+    public List<String> topKFrequent(String[] words, int k)
+    {
+        final PriorityQueue<Map.Entry<String, Long>> priorityQueue = new PriorityQueue<>(COMPARATOR);
+
+        priorityQueue.addAll(
+                Arrays.stream(words)
+                        .collect(Collectors.groupingBy(Function.identity(), Collectors.counting())) // collect frequency of words to a map.
+                        .entrySet()); // add the entry sets to the queue.
+
+        final List<String> result = new LinkedList<>();
+        while (result.size() < k)
+        {
+            result.add(priorityQueue.poll().getKey());
+        }
+
+        return result;
+    }
+}
