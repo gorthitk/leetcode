2c7501c4aa356935ae7545a0de214656b9f9d4af
jetsasank <gteja231989@gmail.com>
2018-04-04 17:47:54 -0400

    Problem 784

---
+++ Problem784/README.md
@@ -0,0 +1,7 @@
+### 783. Minimum Distance Between BST Nodes
+ 
+### Description
+    Given a Binary Search Tree (BST) with the root node root, return the minimum difference between the values of any two different nodes in the tree. 
+   
+### LeetCode URL
+    https://leetcode.com/problems/minimum-distance-between-bst-nodes/description/ 
---
+++ Problem784/Solution.java
@@ -0,0 +1,39 @@
+class Solution
+{
+	public int minDiffInBST(TreeNode root)
+	{
+		return _calculateMin(root, null, null);
+	}
+
+	private int _calculateMin(TreeNode root, Integer lower, Integer upper)
+	{
+		if (root == null)
+		{
+			return 0;
+		}
+
+		int minimumDifference = Integer.MAX_VALUE;
+
+		if (lower != null)
+		{
+			minimumDifference = Math.min(root.val - lower, minimumDifference);
+		}
+
+		if (upper != null)
+		{
+			minimumDifference = Math.min(upper - root.val, minimumDifference);
+		}
+
+		if (root.left != null)
+		{
+			minimumDifference = Math.min(minimumDifference, _calculateMin(root.left, lower, root.val));
+		}
+
+		if (root.right != null)
+		{
+			minimumDifference = Math.min(minimumDifference, _calculateMin(root.right, root.val, upper));
+		}
+
+		return minimumDifference == Integer.MAX_VALUE ? 0 : minimumDifference;
+	}
+}
