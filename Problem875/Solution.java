58a17f57e3978db3b2043009d8525da8b0a274db
tgorthi <gorthitk@gmail.com>
2020-06-11 17:18:51 -0400

    Problem 875

---
+++ Problem875/Solution.java
@@ -0,0 +1,44 @@
+class Solution
+{
+    public int minEatingSpeed(int[] piles, int H)
+    {
+        int max = 0;
+        for (int pile : piles)
+        {
+            max = Math.max(max, pile);
+        }
+
+        int l = 1, r = max;
+
+        while (l < r)
+        {
+            int mid = (r + l) / 2;
+
+            int numberOfHoursNeeded = _calculateHoursNeeded(piles, mid);
+
+            if (numberOfHoursNeeded > H)
+            {
+                l = mid + 1;
+            }
+            else
+            {
+                r = mid;
+            }
+        }
+
+
+        return r;
+    }
+
+    private int _calculateHoursNeeded(int[] piles, int mid)
+    {
+        int hoursNeeded = 0;
+
+        for (int pile : piles)
+        {
+            hoursNeeded += pile / mid + (pile % mid != 0 ? 1 : 0);
+        }
+
+        return hoursNeeded;
+    }
+}
