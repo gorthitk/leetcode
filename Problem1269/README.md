310db5281cc962cd6ad20b6021fd98cb45bff5ed
tgorthi <gorthitk@gmail.com>
2020-06-12 07:20:24 -0400

    Problem 1269

---
+++ Problem1269/README.md
@@ -0,0 +1,11 @@
+### Problem 1269. Number of Ways to Stay in the Same Place After Some Steps
+
+### Description
+    You have a pointer at index 0 in an array of size arrLen. At each step, you can move 1 position to the left, 1 position to the right in the array or stay in the same place  (The pointer should not be placed outside the array at any time).
+    
+    Given two integers steps and arrLen, return the number of ways such that your pointer still at index 0 after exactly steps steps.
+    
+    Since the answer may be too large, return it modulo 10^9 + 7.
+    
+### LeetCode URL
+    https://leetcode.com/problems/number-of-ways-to-stay-in-the-same-place-after-some-steps/
---
+++ Problem1269/Solution.java
@@ -0,0 +1,56 @@
+import java.util.HashMap;
+import java.util.Map;
+
+class Solution
+{
+    int MOD=1000000007;
+    public int numWays(int steps, int arrLen)
+    {
+        Map<Integer, Map<Integer, Long>> cache = new HashMap<>();
+        return numWays(0, steps, arrLen, cache);
+    }
+
+    private int numWays(final int currPos, final int steps, final int arrLen, Map<Integer, Map<Integer, Long>> cache)
+    {
+        if (steps == 0)
+        {
+            return currPos == 0 ? 1 : 0;
+        }
+
+        if (currPos > arrLen - 1 || currPos < 0)
+        {
+            return 0;
+        }
+
+        if (cache.containsKey(currPos) && cache.get(currPos).containsKey(steps))
+        {
+            return (int) (cache.get(currPos).get(steps) % MOD);
+        }
+
+
+        long count = 0;
+        for (int j : new int[]{1, 0, -1})
+        {
+            if (j == 1 && (currPos + 1 > arrLen - 1))
+            {
+                continue;
+            }
+
+            if (j == -1 && (currPos - 1 < 0))
+            {
+                continue;
+            }
+
+            count += numWays(currPos + j, steps - 1, arrLen, cache);
+        }
+
+        cache.computeIfAbsent(currPos, HashMap::new).put(steps, count);
+        return (int) (count % MOD);
+    }
+
+    public static void main(String args[])
+    {
+        Solution sol = new Solution();
+        System.out.println(sol.numWays(27, 7));
+    }
+}
